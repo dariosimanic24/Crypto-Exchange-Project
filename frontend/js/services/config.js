edcbec1865df35a470439e96f4090de0006a1b84
dariosimanic24 <dario.simanic@stu.ibu.edu.ba>
2025-12-29 03:12:27 +0100

    Change API_BASE to localhost for local development

--- frontend/js/services/config.js
+++ frontend/js/services/config.js
@@ -2,7 +2,7 @@
 // You can override window.API_BASE in index.html for deployment.
 // This default keeps localhost working.
 
+
 window.API_BASE =
   window.API_BASE ||
-  "http://164.92.189.93/backend";
-
+  "http://localhost/Crypto-Exchange-Project-milestone-5-final/backend";
